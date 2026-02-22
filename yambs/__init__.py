c2c66b9a3a794f93caec70b9d9df4228ffe719d9
v <vaughn@libre-embedded.com>
2026-02-22 17:46:59 -0600

    3.1.4 - docs release (#100)

--- yambs/__init__.py
+++ yambs/__init__.py
@@ -1,7 +1,7 @@
 # =====================================
 # generator=datazen
 # version=3.2.3
-# hash=6d5e455555b890408f08a63f6226430c
+# hash=998e39c3a9248bf06edf5bdee7f58507
 # =====================================
 
 """
@@ -10,4 +10,4 @@ Useful defaults and other package metadata.
 
 DESCRIPTION = "Yet another meta build-system."
 PKG_NAME = "yambs"
-VERSION = "3.1.3"
+VERSION = "3.1.4"
